22ec92327d2ebb171f1e477d2f6ef525be0fd754
Brandon Zenbook <tangbrandonk@gmail.com>
2024-05-02 22:30:13 -0700

    first commit

---
+++ components/title.tsx
@@ -0,0 +1,10 @@
+export function Title() {
+    return ( 
+        <div className="flex">
+            <div className="text-green-400">visitor@brandon-web.com</div>
+            <div>:</div>
+            <div className="text-blue-400">~</div>
+            <div>$&nbsp;</div>
+        </div>
+     );
+}
